2b6e316db0128c7ebf83eff4d25922afc7ea392a
Israel Alagbe <israelalagbe53@gmail.com>
2021-03-23 14:43:16 +0100

    Attempt to add test

--- tests/_ArrayTest.php
+++ tests/_ArrayTest.php
@@ -1,9 +1,20 @@
 <?php
+
+require('vendor/autoload.php');
+
+// use CustomTypes\_Array;
+
 class _ArrayTest extends \PHPUnit\Framework\TestCase
 {
-public function testAdd()
-   {
-    
-    $this->assertEquals(25, 25);
-   }
+    public function testContructor()
+    {
+        
+        $arr = new CustomTypes\_Array();
+        // $this->assertEquals($arr->toArray(), [], "Array should be empty");
+    }
+    public function testAdd()
+    {
+
+        $this->assertEquals(25, 25);
+    }
 }
