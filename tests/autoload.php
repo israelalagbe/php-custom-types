3a0e4c09f347a86609129f58c87be6c0ecb6aa72
Israel Alagbe <israelalagbe53@gmail.com>
2021-03-23 15:53:29 +0100

    Remove unused values

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,6 +1,5 @@
 <?php
 require_once "./vendor/autoload.php";
-$scannedDirectory = array_diff(scandir("./src/CustomTypes"), array('..', '.'));
 require_once "./src/CustomTypes/_Type.php";
 require_once "./src/CustomTypes/_Iterable.php";
 require_once "./src/CustomTypes/_ArrayObject.php";
