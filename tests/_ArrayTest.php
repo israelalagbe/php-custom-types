d53c1eb58df1760f5c63bc92e908a5db48d38248
Israel Alagbe <israelalagbe53@gmail.com>
2021-03-23 15:52:42 +0100

    Added tests for array

--- tests/_ArrayTest.php
+++ tests/_ArrayTest.php
@@ -2,25 +2,44 @@
 
 require('vendor/autoload.php');
 
-// use CustomTypes\_Array;
+use CustomTypes\_Array;
 
 class _ArrayTest extends \PHPUnit\Framework\TestCase
 {
     public function testContructor()
     {
         
-        $arr = new CustomTypes\_Array();
+        $arr = new _Array();
         $this->assertEquals($arr->toArray(), [], "Array should be empty");
 
-        $arr = new CustomTypes\_Array([1,2,3]);
+        $arr = new _Array([1,2,3]);
         $this->assertEquals($arr->toArray(), [1,2,3], "Array should be equal");
 
-        $arr = new CustomTypes\_Array(1,2,3,4);
+        $arr = new _Array(1,2,3,4);
         $this->assertEquals($arr->toArray(), [1,2,3,4], "Array should be equal");
     }
-    public function testAdd()
-    {
 
-        $this->assertEquals(25, 25);
+    public function testGetter()
+    {
+        $arr = new _Array([
+            'item' => 'value'
+        ]);
+        $this->assertEquals($arr->item, 'value');
+    }
+
+    public function testSetter()
+    {
+        $arr = new _Array();
+
+        try{
+            $arr->hello;
+            $this->fail("Expected exception not thrown");
+        }
+        catch(Exception $e) {
+            
+        }
+
+        $arr->hello = 'world';
+        $this->assertEquals($arr->hello, 'world');
     }
 }
