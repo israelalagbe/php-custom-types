227d8066ffcaf99a6245b395bd6bfdadca2e33c2
Israel Alagbe <israelalagbe53@gmail.com>
2021-05-28 15:12:00 +0100

    Added a first and last function

--- tests/_ArrayTest.php
+++ tests/_ArrayTest.php
@@ -39,4 +39,22 @@ class _ArrayTest extends \PHPUnit\Framework\TestCase
         $arr->hello = 'world';
         $this->assertEquals($arr->hello, 'world');
     }
+
+    public function testFirst()
+    {
+        $arr = new _Array([1,2,3]);
+        $this->assertEquals($arr->first(), 1, "Should return the first item");
+
+        $arr = new _Array([]);
+        $this->assertEquals($arr->first(), false, "Should return false when there are no items.");
+    }
+
+    public function testLast()
+    {
+        $arr = new _Array(['a', 'b', 'c']);
+        $this->assertEquals($arr->last(), 'c', "Should return the last item");
+
+        $arr = new _Array([]);
+        $this->assertEquals($arr->last(), false, "Should return false when there are no items.");
+    }
 }
